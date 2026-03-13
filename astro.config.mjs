3fdd85dfd79eee6486109d2b7c10bfad30b7733a
Kamil <klawrynowicz@edu.cdv.pl>
2026-03-13 14:00:45 +0100

    nowa strona

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+const site = process.env.PUBLIC_SITE_URL || 'https://example.com';
+const base = process.env.PUBLIC_BASE_PATH || '/';
+
+export default defineConfig({
+  site,
+  base,
+});
